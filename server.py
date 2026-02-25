4eb76d2325f6d833b2d1117223778a42e5ce20b6
nite29 <ardjunsay29@gmail.com>
2026-02-25 12:50:17 +0800

    Update server.py

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ def start_server(host, port):
     while True:
         raw_bytes, client_addr = sock.recvfrom(CHUNK_SIZE)
         conn = establish_connection(sock, client_addr, raw_bytes)
-        if conn == -1:
+        if conn == 0:
             print(f"Could not establish connection with {client_addr}.")
             return
         if conn == 1:
@@ -22,16 +22,18 @@ def start_server(host, port):
 
 
 # implementation of 3 way handshake from tcp
-# returns 1 if successful, returns -1 if not
+# returns 1 if successful, returns 0 if not
 def establish_connection(sock, client_addr, raw_bytes):
     msg = parse_packet(raw_bytes)
     server_isn = generate_isn()
-    expected_ack = server_isn + 1
-    try:
+    expected_ack = server_isn + 1   
+
+    for attempt in range(1, MAX_RETRIES + 1):
         syn_ack = build_syn_ack(server_isn, msg["seq"])
         sock.sendto(syn_ack, client_addr)
-        sock.settimeout(TIMEOUT)
-        while True:
+        print(f"[SYN-ACK] Sent SEQ={server_isn} (attempt {attempt})")
+        try:
+            sock.settimeout(TIMEOUT)
             recv_bytes, addr = sock.recvfrom(CHUNK_SIZE)
             packet = parse_packet(recv_bytes)
             if packet["type"] == "ACK":
@@ -40,9 +42,12 @@ def establish_connection(sock, client_addr, raw_bytes):
                     return 1
                 else:
                     print(f"Handshake failed with {addr}")
-                    return -1
+                    return 0
 
-    except socket.error:
-        print("Handshake timed out.")
-    finally:
-        sock.settimeout(None)
+        except socket.error:
+            print("Client no response, trying again.")
+            continue
+        finally:
+            print("Handshake reached max retries.")
+            sock.settimeout(None)
+    return 0
